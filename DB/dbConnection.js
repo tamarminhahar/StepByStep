5da8bc2dd0049e7c19c18a8ff39f68eeab002161
tamarminhahar <tamarminhahar@gmail.com>
2025-06-10 14:53:10 +0300

    My local changes to dbConnection.js

--- DB/dbConnection.js
+++ DB/dbConnection.js
@@ -1,26 +1,22 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-
 dotenv.config();
+console.log('env vars:', {
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+});
+const db = await mysql.createPool({
 
-// Function to create and return a database connection pool
-let db;
-
-try {
-  db = mysql.createPool({
-    host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 3000,
-    database: process.env.DB_NAME || 'grief_support',
-    multipleStatements: true
+    host:'127.0.0.1',
+    user:  'root',
+    password: process.env.DB_PASSWORD || '327880845',
+    database:'grief_support', 
+    multipleStatements: true 
   });
 
-  // Optional: test the connection
-  const connection = await db.getConnection();
-  console.log(' Connected to MySQL as:', process.env.DB_USER);
-  connection.release();
-} catch (error) {
-  console.error(' Failed to connect to MySQL:', error.message);
-}
+  
+  console.log(`Connected to database$`);
 
 export default db;
